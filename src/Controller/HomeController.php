dfb986996f7ca7486c9b967f1347284cc39c0c0c
La Piscine <lapiscine@macbook-air-de-la.home>
2020-03-20 17:18:11 +0100

    épuration

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,56 +11,55 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController{
 
-    /**
-     * @Route("/", name="accueil")
-     */
+    private $articles = [
 
-    public function accueil(){
-
-        $article = [
-            1 => [
-                'title' => 'titre article 1',
-                'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-                'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMiF_QYKvu_SgiciSMJ7AwfSOl7I_VLJAC_23h9X102-sKZP_3',
-                'published' => true
+        1 => [
+            'title' => 'titre article 1',
+            'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+            'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMiF_QYKvu_SgiciSMJ7AwfSOl7I_VLJAC_23h9X102-sKZP_3',
+            'published' => true
             ],
-            2 => [
-                'title' => 'titre article 2',
-                'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-                'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMiF_QYKvu_SgiciSMJ7AwfSOl7I_VLJAC_23h9X102-sKZP_3',
-                'published' => true
+        2 => [
+            'title' => 'titre article 2',
+            'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+            'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMiF_QYKvu_SgiciSMJ7AwfSOl7I_VLJAC_23h9X102-sKZP_3',
+            'published' => true
             ],
-            3 => [
-                'title' => 'titre article 3',
-                'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-                'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMiF_QYKvu_SgiciSMJ7AwfSOl7I_VLJAC_23h9X102-sKZP_3',
-                'published' => false
+        3 => [
+            'title' => 'titre article 3',
+            'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+            'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMiF_QYKvu_SgiciSMJ7AwfSOl7I_VLJAC_23h9X102-sKZP_3',
+            'published' => false
             ],
-            4 => [
-                'title' => 'titre article 4',
-                'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-                'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMiF_QYKvu_SgiciSMJ7AwfSOl7I_VLJAC_23h9X102-sKZP_3',
-                'published' => false
+        4 => [
+            'title' => 'titre article 4',
+            'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+            'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMiF_QYKvu_SgiciSMJ7AwfSOl7I_VLJAC_23h9X102-sKZP_3',
+            'published' => false
             ],
-            5 => [
-                'title' => 'titre article 5',
-                'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-                'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMiF_QYKvu_SgiciSMJ7AwfSOl7I_VLJAC_23h9X102-sKZP_3',
-                'published' => true
+        5 => [
+            'title' => 'titre article 5',
+            'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+            'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMiF_QYKvu_SgiciSMJ7AwfSOl7I_VLJAC_23h9X102-sKZP_3',
+            'published' => true
             ],
-            6 => [
-                'title' => 'titre article 6',
-                'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-                'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMiF_QYKvu_SgiciSMJ7AwfSOl7I_VLJAC_23h9X102-sKZP_3',
-                'published' => true
+        6 => [
+            'title' => 'titre article 6',
+            'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+            'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMiF_QYKvu_SgiciSMJ7AwfSOl7I_VLJAC_23h9X102-sKZP_3',
+            'published' => true
             ]
         ];
 
+    /**
+     * @Route("/", name="accueil")
+     */
+    public function accueil(){
 
 
         return $this->render('accueil.html.twig', [
 
-            'article' => $article,
+            'articles' => $this -> articles,
         ]);
 
     }
@@ -71,49 +70,8 @@ class HomeController extends AbstractController{
      */
     public function blog($id)
     {
-        $article = [
-            1 => [
-                'title' => 'titre article 1',
-                'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-                'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMiF_QYKvu_SgiciSMJ7AwfSOl7I_VLJAC_23h9X102-sKZP_3',
-                'published' => true
-            ],
-            2 => [
-                'title' => 'titre article 2',
-                'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-                'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMiF_QYKvu_SgiciSMJ7AwfSOl7I_VLJAC_23h9X102-sKZP_3',
-                'published' => true
-            ],
-            3 => [
-                'title' => 'titre article 3',
-                'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-                'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMiF_QYKvu_SgiciSMJ7AwfSOl7I_VLJAC_23h9X102-sKZP_3',
-                'published' => false
-            ],
-            4 => [
-                'title' => 'titre article 4',
-                'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-                'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMiF_QYKvu_SgiciSMJ7AwfSOl7I_VLJAC_23h9X102-sKZP_3',
-                'published' => false
-            ],
-            5 => [
-                'title' => 'titre article 5',
-                'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-                'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMiF_QYKvu_SgiciSMJ7AwfSOl7I_VLJAC_23h9X102-sKZP_3',
-                'published' => true
-            ],
-            6 => [
-                'title' => 'titre article 6',
-                'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-                'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMiF_QYKvu_SgiciSMJ7AwfSOl7I_VLJAC_23h9X102-sKZP_3',
-                'published' => true
-            ]
-        ];
-
-        $articles = $article[$id];
-
         return $this->render('article.html.twig', [
-            'articles' => $articles
+            'article' => $this ->articles[$id]
         ]);
 
     }
