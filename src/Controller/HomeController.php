1ee3b3c2b772f42cca07a75c8a602d46ea6d2531
La Piscine <lapiscine@macbook-air-de-la.home>
2020-03-20 15:15:17 +0100

    correction lien

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,7 +4,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,7 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController{
 
     /**
-     * @Route{"/", name = "accueil"}
+     * @Route("/", name="accueil")
      */
 
     public function accueil(){
